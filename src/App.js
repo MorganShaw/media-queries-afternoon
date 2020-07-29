317c678abec933f2b468794095f8bc0c75a19c3e
user-1 <user-1@MacBook-Pro.local>
2020-07-29 17:13:49 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
+// class App extends Component() {
+  // constructor() {
+  //   super()
+  //   this.state = {
+
+  //   }
+  // }
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // render (){
+    return (
+      <div className="App">
+        <div className="welcome-page">
+          <header>
+            <div className="nav-container">
+              <a class="navbar-brand" href="#page-top">
+                <img src="https://startbootstrap.github.io/startbootstrap-agency/assets/img/navbar-logo.svg"/>
+              </a>
+              <div className="desktop-menu">
+                <p className="menu-link">SERVICES</p>
+                <p className="menu-link">PORTFOLIO</p>
+                <p className="menu-link">ABOUT</p>
+                <p className="menu-link">TEAM</p>
+                <p className="menu-link">CONTACT</p>
+              </div>
+              <div>
+                <img className="hamburger" src="./download.png"/>
+              </div>
+            </div>
+          </header>
+          <div className="welcome-container">
+            <h2 className="welcome-1">Welcome To Our Studio!</h2>
+            <h2 className="welcome-2">IT'S NICE TO MEET YOU</h2>
+            <button className="more-btn" href="#services">TELL ME MORE</button>
+          </div>
+        </div>
+      </div>
+    );
+
+  // }
 }
 
 export default App;
